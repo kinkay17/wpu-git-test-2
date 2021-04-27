9700fdf899b11baeeafa9dd356e05152adb1584c
kinkay17 <kinkay17@gmail.com>
2021-04-28 06:51:47 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
 <body>
     <h1>Hello World</h>  
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta saepe pariatur, totam dicta recusandae asperiores quaerat ex error harum repellendus eum eligendi atque distinctio. Amet debitis ex omnis dolorum libero.</p>
-    
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>
 </body>
 </html>
